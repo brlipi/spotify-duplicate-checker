8535d783222640d7eef05205601ef59c8252a45d
Breno Lipi <brenorlipi@hotmail.com>
2021-02-22 01:39:34 -0300

    Page redirection

--- views/script.js
+++ views/script.js
@@ -3,6 +3,7 @@ var elements = document.querySelectorAll("a");
 for (var i = 0; i < elements.length; i++)
 {
     elements[i].addEventListener("click", function() {
+        window.location.href = `http://localhost:8888/playlists/${this.id}`;
         console.log(this.id);
 
         fetch(`/playlists/${this.id}`, {
