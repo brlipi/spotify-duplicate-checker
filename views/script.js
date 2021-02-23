617e6df856ff4c80314ce7f5d70d0baa6bb7e17e
Breno Lipi <brenorlipi@hotmail.com>
2021-02-23 17:07:18 -0300

    Removed fetch

--- views/script.js
+++ views/script.js
@@ -3,13 +3,6 @@ var elements = document.querySelectorAll("a");
 for (var i = 0; i < elements.length; i++)
 {
     elements[i].addEventListener("click", function() {
-        window.location.href = `http://localhost:8888/playlists/${this.id}`;
-        console.log(this.id);
-
-        fetch(`/playlists/${this.id}`, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-        });
-        console.log('test');
+        window.location.href = `http://localhost:8888/playlists/${this.id}`;        
     }, true);
 }
