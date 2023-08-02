274541a1d74acfe9f4c3c01a716975d170a869ee
Breno Lipi <brenorlipi@hotmail.com>
2023-08-02 17:57:48 -0300

    Change from fetch to axios

--- src/utils/dataFetching.ts
+++ src/utils/dataFetching.ts
@@ -1,15 +1,16 @@
 import axios from 'axios'
 
-export async function fetchPlaylists(token: string) {
+export async function fetchPlaylists(access_token: string) {
     try {
         const { data } = await axios.get('https://api.spotify.com/v1/me/playlists?limit=50', {
             headers: {
-                'Authorization': `Bearer ${token}`
+                'Authorization': `Bearer ${access_token}`
             }
         });
         return data;
     } catch (error) {
         console.log(error);
+        return null;
     }
 }
 
@@ -27,24 +28,37 @@ export async function fetchFromPlaylist(access_token: string, playlistId: string
 }
 
 async function getPlaylistSize(access_token: string, playlistId: string) {
-    const result = await fetch(
-        `https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=total%2Climit`,
-        {
-            method: "GET", headers: { Authorization: `Bearer ${access_token}` }
+    try {
+        const { data } = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?fields=total%2Climit`, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
         });
-
-    const { total, limit } = await result.json();
-
-    return { total, limit };
+        return data;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
-async function fetchTracks(code: string, playlistId: string, limit: number, offset: number) {
-    const result = await fetch(
-        // Can something like URLSearchParams be used so I don't have a MASSIVE url on the editor?
-        `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}&offset=${offset}&fields=items%28added_at%2Cadded_by%2Cis_local%2Ctrack%28id%2Cname%2Calbum.name%2Cartists%29%29`,
-        {
-            method: "GET", headers: { Authorization: `Bearer ${code}` }
-        });
+async function fetchTracks(access_token: string, playlistId: string, limit: number, offset: number) {
+    try {
+        const {data} = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}&offset=${offset}`, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        })
+        return data;
+    } catch(error) {
+        console.log(error);
+        return null;
+    }
+    // const result = await fetch(
+    //     // Can something like URLSearchParams be used so I don't have a MASSIVE url on the editor?
+    //     `https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=${limit}&offset=${offset}&fields=items%28added_at%2Cadded_by%2Cis_local%2Ctrack%28id%2Cname%2Calbum.name%2Cartists%29%29`,
+    //     {
+    //         method: "GET", headers: { Authorization: `Bearer ${access_token}` }
+    //     });
 
-    return await result.json();
+    // return await result.json();
 }
