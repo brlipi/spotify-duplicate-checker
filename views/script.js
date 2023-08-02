fe9671e1a5ffba15e6959a48686f6e872aa05736
Breno Lipi <brenorlipi@hotmail.com>
2023-08-01 22:51:49 -0300

    Create electron-vite

--- views/script.js
+++
@@ -1,8 +0,0 @@
-var elements = document.querySelectorAll("a");
-
-for (var i = 0; i < elements.length; i++)
-{
-    elements[i].addEventListener("click", function() {
-        window.location.href = `http://localhost:8888/playlists/${this.id}`;        
-    }, true);
-}
