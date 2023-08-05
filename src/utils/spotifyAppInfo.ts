1f25714395ea48f788616728d2ff8889f6209b66
Breno Lipi <brenorlipi@hotmail.com>
2023-08-05 13:17:50 -0300

    Add .env file to set client id and redirect uri

--- src/utils/spotifyAppInfo.ts
+++ src/utils/spotifyAppInfo.ts
@@ -1,4 +1,4 @@
-const client_id = '593691e7e7a24881ac395724c20b9a78';
-const redirect_uri = 'http://localhost:5173/callback';
+const client_id = import.meta.env.VITE_CLIENT_ID;
+const redirect_uri = import.meta.env.VITE_REDIRECT_URI;
 
 export { client_id, redirect_uri }
